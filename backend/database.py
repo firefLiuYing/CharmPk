6a2125903df5a44d892198e14e58d409218b1d79
鸟 月月 <1689759238@qq.com>
2025-06-14 09:26:26 +0800

    声明了数据库对外接口方法，添加了user文件用于用户管理

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,27 @@
 # 该文件对外提供数据库相关方法
+from flask import jsonify
+
+def write_user(user_info):
+    user_id=0
+    return user_id
+def read_user(user_id):
+    user_info=jsonify({"id":user_id})
+    return user_info
+def write_post(post_info):
+    post_id=0
+    return post_id
+def read_post(post_id):
+    post_info=jsonify({"id":post_id})
+    return post_info
+def write_image(image_info):
+    image_id=0
+    return image_id
+def read_image(image_id):
+    image_info=jsonify({"id":image_id})
+    return image_info
+def write_comment(comment_info):
+    comment_id=0
+    return comment_id
+def read_comment(comment_id):
+    comment_info=jsonify({"id":comment_id})
+    return comment_info
--- backend/face_ai.py
+++ backend/face_ai.py
@@ -1,5 +1,5 @@
 # 该文件对外提供图片分类，打分方法
 
-
-def calculate_score(face_img):
-    return 80.0
+def calculate_image(face_img):
+    img_id=0
+    return img_id
