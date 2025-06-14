8373b5250f26aefe718b4f8c36d32cfcf4ec9a1f
鸟 月月 <1689759238@qq.com>
2025-06-14 10:47:03 +0800

    写了创建数据库方法

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
 # 该文件对外提供数据库相关方法
 from flask import jsonify
+import sqlite3
 
 def write_user(user_info):
     user_id=0
@@ -25,3 +26,59 @@ def write_comment(comment_info):
 def read_comment(comment_id):
     comment_info=jsonify({"id":comment_id})
     return comment_info
+
+def create_database():
+    conn=sqlite3.connect('charm_pk.db')
+    cursor=conn.cursor()
+
+    # 创建用户表
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   -- 用户ID
+        name TEXT NOT NULL UNIQUE,              -- 用户唯一标识（用户名）
+        secret_key TEXT NOT NULL,               -- 用户密码
+        posts TEXT,                             -- 用户发布的帖子ID列表
+        images TEXT,                            -- 用户上传的图片ID列表
+        comments TEXT,                          -- 用户上传的评论ID列表
+        friends TEXT,                           -- 用户的好友ID列表
+    );
+    ''')
+
+    # 创建帖子表
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS post(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   -- 帖子ID
+        title TEXT NOT NULL,                    -- 帖子标题
+        content TEXT,                           -- 帖子内容
+        poster INTEGER,                         -- 发布者ID
+        comments TEXT,                          -- 评论ID列表
+        thumbs INTEGER DEFAULT 0,               -- 点赞数
+    );
+    ''')
+
+    # 创建图片表
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS image(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   -- 图片ID
+        data BLOB,                              -- 图片数据
+        score INTEGER,                          -- 图片颜值
+        sex TEXT,                               -- 性别
+        age TEXT,                               -- 年龄段
+        emoji TEXT,                             -- 表情
+        face_shape TEXT,                        -- 脸型
+    );
+    ''')
+
+    # 创建评论表
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS comment(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   -- 评论ID
+        commenter INTEGER,                      -- 评论发布者ID
+        content TEXT,                           -- 评论内容
+        thumbs INTEGER DEFAULT 0,               -- 评论点赞数
+    );
+    ''')
+
+    # 提交操作并关闭连接
+    conn.commit()
+    conn.close()
