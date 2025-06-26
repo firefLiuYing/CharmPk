e186f3d1830c1c033025015d841be8b9074c4638
sunnystrike <141255826+sunnystrike@users.noreply.github.com>
2025-06-26 10:24:02 +0800

    首页UI框架优化

--- src/App.vue
+++ src/App.vue
@@ -43,16 +43,18 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
 
 <style>
-  .image-container img {
+  .image-container img{
+    display: block;
     width: 100px; /* 设置宽度 */
     height: 100px; /* 设置高度 */
     object-fit: cover; /* 保持图片比例 */
+    margin-left: auto;
+    margin-right: auto;
   }
 </style>
--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -1,26 +1,35 @@
 <!-- HomePage.vue -->
 <template>
   <div class="container">
-    <div class="top-left">
-        <div class="tl-img">
-         <img src = "@/assets/setting.png" alt="Setting" :style="imageStyle">
+    <div class="left">
+        <div class="top-left">
+          <div class="tl-img">
+           <img src = "@/assets/setting.png" alt="Setting" :style="imageStyle">
+          </div>
+          <div class="tl-text">
+            粉色默剧
+          </div>
         </div>
-        <div class="tl-text">
-          粉色默剧
+        <div class="bottom-left">
+          <div class="left-side-bar">
+            颜值评分
+          </div>
+          <div class="left-side-bar">
+            颜值PK
+          </div>
+          <div class="left-side-bar">
+              社交空间
+          </div>
+          <div class="left-side-bar">
+              好友列表
+          </div>
         </div>
     </div>
 
-    <div class="top-right">
-      1231321
+    <div class="right">
+        12213
     </div>
 
-    <div class="bottom-left">
-      1231321
-    </div>
-
-    <div class="bottom-right">
-      1231321
-    </div>
 
   </div>
 
@@ -40,41 +49,89 @@ const imageStyle = ref({
 .container {
   display: grid;
   grid-template-columns: 1fr 3fr;
-  grid-template-rows: 1fr 4fr;
+  grid-template-rows: auto;
   gap: 4px;
   height: 100vh;
   padding: 16px;
   box-sizing: border-box;
 }
 
-.top-left {
+.left {
+  grid-column: 1;
+  grid-row: 1;
+  flex: 1;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  background-color: #f9f9f9;
+  justify-content: flex-start; /* 水平方向起始对齐 */
+
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: 1fr 3fr;
+}
+.top-left{
   grid-column: 1;
   grid-row: 1;
   border: 1px solid #e0e0e0;
   border-radius: 8px;
   background-color: #f9f9f9;
-  align-items: center;
-  justify-content: flex-start; /* 水平方向起始对齐 */
-  padding-left: 10px; /* 图片和文字之间的间距 */
+  justify-content: flex-start;
+
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: auto;
 }
 
-.tl-img{
-  margin: 5px;
+.tl-img {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  width: 100px; /* 容器宽度 */
+  height: 100px; /* 容器高度 */
+  border-radius: 50%; /* 圆形容器 */
+  overflow: hidden; /* 隐藏超出圆形部分 */
+}
 
+.tl-img img{
+  width: 100%; /* 图片宽度填满容器 */
+  height: auto; /* 图片高度自动 */
+  border-radius: 50%; /* 图片也设置为圆形 */
 }
 
 .tl-text{
-  flex-grow: 1;
+  flex: 2; /* 占据三分之二的空间 */
+  justify-content: flex-start; /* 水平方向起始对齐 */
+  align-content: center;
+  font-size: 20px; /* 设置字体大小为 20px */
+  font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 
-.top-right {
+.right {
   grid-column: 2;
   grid-row: 1;
+  flex: 2;
   border: 1px solid #e0e0e0;
   border-radius: 8px;
   padding: 16px;
   background-color: #f9f9f9;
 }
+
+/*
+.top-side-bar{
+  flex: 1;
+  justify-content: center;
+  align-content: center;
+  font-size: 20px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #787676;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  padding: 30px;
+  background-color: #17faa2;
+}
+*/
+
 
 .bottom-left {
   grid-column: 1;
@@ -83,8 +140,27 @@ const imageStyle = ref({
   border-radius: 8px;
   padding: 16px;
   background-color: #f9f9f9;
+
+  display: grid;
+  grid-template-columns: auto; /* 定义两列 */
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr; /* 定义五行 */
 }
 
+.left-side-bar{
+  flex: 1; /* 占据三分之二的空间 */
+  justify-content: center; /* 水平方向起始对齐 */
+  align-content: center;
+  font-size: 20px; /* 设置字体大小为 20px */
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #686565;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  padding: 16px;
+  background-color: #17faa2;
+}
+
+
+/*
 .bottom-right {
   grid-column: 2;
   grid-row: 2;
@@ -97,9 +173,6 @@ const imageStyle = ref({
   align-items: center;
   justify-content: center;
 }
-
-.side-bar{
-
-}
+*/
 
 </style>
